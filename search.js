17fbc55e440bb9b971fc84bb6ea3c0474722400d
u9g <43508353+u9g@users.noreply.github.com>
2021-04-26 09:59:18 -0400

    Update search.js

--- search.js
+++ search.js
@@ -9,8 +9,9 @@ function search (md, input, type) {
   let searchTerm = input
   if (type === 'function') searchTerm = `${searchTerm}(`
   if (type === 'event') searchTerm = `"${searchTerm}"`
-
+  const firstLineAfterTOC = md.indexOf('<h1 id="api">API</h1>')
   const lines = md
+    .filter((_, ix) => ix < firstLineAfterTOC) // dont get table of contents in search results
     .map(o => o.replace(/&quot;/g, '"'))
     .map(o => o.replace(/&gt;/g, '<'))
     .map(o => o.replace(/&lt;/g, '>'))
@@ -18,7 +19,6 @@ function search (md, input, type) {
     .map(o => o.replace(/&ge;/g, '≥'))
     .filter(line => line.startsWith('<li>'))
     .filter(line => line.includes(searchTerm) && line.includes('href'))
-  console.log(lines.join('\n'))
   const slugs = lines.map(o => o.match(/^<li><a href="((?:[a-zA-Z0-9]|-|#|_|\.)+)">(.+)<\/a><(?:\/li|ul)>(?: +)?$/))
   const links = slugs
     .map((s, ix) => `[${s[2]}](https://github.com/PrismarineJS/mineflayer/blob/master/docs/api.md${s[1]})`)
