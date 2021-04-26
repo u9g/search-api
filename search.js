7dc1176993fedd73827d9d8ed435ab7c48f3b558
u9g <43508353+u9g@users.noreply.github.com>
2021-04-26 09:46:56 -0400

    Create search.js

---
+++ search.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @param {string} searchTerm
+ * @param {undefined | 'function' | 'event'} type
+ * @returns
+ */
+function search (md, input, type) {
+  if (type === 'all') type = undefined
+  let searchTerm = input
+  if (type === 'function') searchTerm = `${searchTerm}(`
+  if (type === 'event') searchTerm = `"${searchTerm}"`
+
+  const lines = md
+    .map(o => o.replace(/&quot;/g, '"'))
+    .map(o => o.replace(/&gt;/g, '<'))
+    .map(o => o.replace(/&lt;/g, '>'))
+    .map(o => o.replace(/&le;/g, '≤'))
+    .map(o => o.replace(/&ge;/g, '≥'))
+    .filter(line => line.startsWith('<li>'))
+    .filter(line => line.includes(searchTerm) && line.includes('href'))
+  console.log(lines.join('\n'))
+  const slugs = lines.map(o => o.match(/^<li><a href="((?:[a-zA-Z0-9]|-|#|_|\.)+)">(.+)<\/a><(?:\/li|ul)>(?: +)?$/))
+  const links = slugs
+    .map((s, ix) => `[${s[2]}](https://github.com/PrismarineJS/mineflayer/blob/master/docs/api.md${s[1]})`)
+  return links
+}
+
+module.exports = search
